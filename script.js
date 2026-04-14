df7b87afcf15f6745988204934978de8a13f40c4
salmansalman19 <salman_ss19@hotmail.com>
2026-04-14 23:44:23 +0300

    Add Contact Form

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ toggleBtn.addEventListener("click", function () {
     localStorage.setItem("theme", "light");
   }
 });
+
+let nameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let messageInput = document.getElementById("message");
+let sendBtn = document.getElementById("sendBtn");
+let error = document.getElementById("error");
+
+sendBtn.addEventListener("click", function () {
+  let name = nameInput.value.trim();
+  let email = emailInput.value.trim();
+  let message = messageInput.value.trim();
+
+  if (name === "" || email === "" || message === "") {
+    error.innerText = "All fields are required!";
+    error.style.color = "red";
+  } else if (!email.includes("@")) {
+    error.innerText = "Please enter a valid email!";
+    error.style.color = "red";
+  } else {
+    error.innerText = "Message sent successfully ✅";
+    error.style.color = "green";
+  }
+});
+nameInput.value = "";
+emailInput.value = "";
+messageInput.value = "";
