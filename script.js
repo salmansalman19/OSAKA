8b3d572d8f69d055d12a1559c23a0887fdc232ca
salmansalman19 <salman_ss19@hotmail.com>
2026-04-14 23:30:31 +0300

    Add Dark Mode feature

---
+++ script.js
@@ -0,0 +1,17 @@
+let toggleBtn = document.getElementById("darkModeToggle");
+
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+
+toggleBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
